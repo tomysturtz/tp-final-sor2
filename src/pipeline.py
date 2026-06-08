6f4df6f9688827599b4f24d1a15095d10a0f9534
Tomas Sturtz <sturtz.tomas@gmail.com>
2026-06-08 17:20:18 -0300

    Se agregan resultados de 2 prompts de los escenarios 2 y 5

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,17 +2,16 @@ import requests
 import json
 import csv
 import time
-import os
 
 # Configuración
 OLLAMA_URL = 'http://localhost:11434/api/generate'
 # phi3:mini o llama3.2:3b
 MODEL = 'phi3:mini'
-ESCENARIO = 'escenario1'
+ESCENARIO = 'escenario2'
 # Elegir formato a probar: 'raw_completo', 'raw_reducido' o 'json_reducido'
-FORMATO_ENTRADA = 'raw_reducido'
+FORMATO_ENTRADA = 'raw_completo'
 # Elegir entre 'prompt1','prompt2','prompt3'
-PROMPT_UTILIZAR = 'prompt3'
+PROMPT_UTILIZAR = 'prompt2'
 
 if MODEL == 'phi3:mini':
     modelo = 'phi3mini'
